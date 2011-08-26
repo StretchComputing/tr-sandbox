4d71a249a904e08055a145ba7c28ac38a7fd9eb4
Terry Roe <terryroe@gmail.com>
2011-08-26 10:24:58 -0400

    initial commit

---
+++ src/com/stretchcom/sandbox/Tr_sandboxServlet.java
@@ -0,0 +1,13 @@
+package com.stretchcom.sandbox;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Tr_sandboxServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
